01fd0ac4882be597ade2450cd57a426e89b8d3fe
Safir <safirben12@gmail.com>
2024-06-29 03:07:55 +0200

    azy marche un peu ?

--- public/portfolio.js
+++ public/portfolio.js
@@ -1,63 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     const portfolioContainer = document.getElementById('portfolio-container');
 
-    const projects = [
-        {
-            folder: 'Émotions Animées',
-            title: 'Émotions animées',
-            description: 'Ici les émotions prennent la dimension du plan matérielle avec lequel nous les ressentons et les exprimons pour palier aux failles du langage et renforcer notre communication picturale.'
-        },
-        {
-            folder: 'Abstracswag',
-            title: 'AbstracSwag',
-            description: 'Etre ou ne pas être, telle est l\'abswagction.'
-        },
-        {
-            folder: 'Portraits d\'êtres vivants du Quotidien',
-            title: 'Portraits d\'êtres vivants du Quotidien',
-            description: 'L\'univers et multiple, plein de personnalité qui communiquent et ne font qu\'un par la diversité et les liens qu\'elles tissent. Voici leur histoire.'
-        },
-        {
-            folder: 'Éléments de peinture naturaliste',
-            title: 'Éléments de peinture naturaliste',
-            description: 'Travail d\'archivage des paysages de notre époque à l\'aide de techniques très précises de reproduction par le dessin afin de donner une idée d\'à quoi ressemblait notre belle planète aux générations futures qui ne connaitront plus l\'apparence de l\'herbe verte.'
-        }
-    ];
+    fetch('/paints/')
+        .then(response => response.text())
+        .then(text => {
+            const parser = new DOMParser();
+            const htmlDocument = parser.parseFromString(text, "text/html");
+            const folderElements = htmlDocument.querySelectorAll("a");
+            folderElements.forEach(element => {
+                const folderName = element.getAttribute("href").replace('/', '');
+                if (folderName) {
+                    const projectElement = document.createElement('div');
+                    projectElement.classList.add('portfolio-item');
+                    
+                    const thumbnail = `/paints/${folderName}/image1.png`;
+                    projectElement.innerHTML = `
+                        <img src="${thumbnail}" alt="${folderName}">
+                        <p>${folderName}</p>
+                    `;
 
-    projects.forEach(project => {
-        const projectElement = document.createElement('div');
-        projectElement.classList.add('portfolio-item');
+                    projectElement.addEventListener('click', () => {
+                        window.location.href = `project.html?folder=${encodeURIComponent(folderName)}`;
+                    });
 
-        // Load the first image in the folder as the thumbnail
-        const thumbnail = `/${project.folder}/` + getFirstImage(project.folder);
-        projectElement.innerHTML = `
-            <img src="${thumbnail}" alt="${project.title}">
-            <p>${project.title}</p>
-        `;
-
-        projectElement.addEventListener('click', () => {
-            window.location.href = `project.html?folder=${encodeURIComponent(project.folder)}`;
-        });
-
-        portfolioContainer.appendChild(projectElement);
-    });
-
-    function getFirstImage(folder) {
-        let firstImage = 'default-thumbnail.png'; // default image in case the folder is empty
-        fetch(`/${folder}/`)
-            .then(response => response.text())
-            .then(text => {
-                const parser = new DOMParser();
-                const htmlDocument = parser.parseFromString(text, "text/html");
-                const imageElements = htmlDocument.querySelectorAll("a");
-                imageElements.forEach(element => {
-                    const fileName = element.getAttribute("href");
-                    if (fileName.match(/\.(jpg|jpeg|png|gif|png)$/)) {
-                        firstImage = fileName;
-                        return false; // Break the loop
-                    }
-                });
+                    portfolioContainer.appendChild(projectElement);
+                }
             });
-        return firstImage;
-    }
+        });
 });
--- public/project.js
+++ public/project.js
@@ -2,51 +2,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(window.location.search);
     const folder = urlParams.get('folder');
 
-    const projectDetails = {
-        'Émotions Animées': {
-            title: 'Émotions animées',
-            description: 'Ici les émotions prennent la dimension du plan matérielle avec lequel nous les ressentons et les exprimons pour palier aux failles du langage et renforcer notre communication picturale.'
-        },
-        'Abstracswag': {
-            title: 'AbstracSwag',
-            description: 'Etre ou ne pas être, telle est l\'abswagction.'
-        },
-        'Portraits d\'êtres vivants du Quotidien': {
-            title: 'Portraits d\'êtres vivants du Quotidien',
-            description: "L'univers et multiple, plein de personnalité qui communiquent et ne font qu'un par la diversité et les liens qu'elles tissent. Voici leur histoire."
-        },
-        'Éléments de peinture naturaliste': {
-            title: 'Éléments de peinture naturaliste',
-            description: "Travail d'archivage des paysages de notre époque à l'aide de techniques très précises de reproduction par le dessin afin de donner une idée d'à quoi ressemblait notre belle planète aux générations futures qui ne connaitront plus l'apparence de l'herbe verte."
-        }
-    };
+    document.getElementById('project-title').textContent = folder;
 
-    const project = projectDetails[folder];
+    const projectGallery = document.getElementById('project-gallery');
 
-    if (project) {
-        document.getElementById('project-title').textContent = project.title;
-        document.getElementById('project-description').textContent = project.description;
-
-        const projectGallery = document.getElementById('project-gallery');
-        
-        // Fetch all images from the folder
-        fetch(`/${folder}/`)
-            .then(response => response.text())
-            .then(text => {
-                const parser = new DOMParser();
-                const htmlDocument = parser.parseFromString(text, "text/html");
-                const imageElements = htmlDocument.querySelectorAll("a");
-                imageElements.forEach(element => {
-                    const fileName = element.getAttribute("href");
-                    if (fileName.match(/\.(jpg|jpeg|png|gif|png)$/)) {
-                        const imgElement = document.createElement('img');
-                        imgElement.src = `/${folder}/${fileName}`;
-                        imgElement.alt = `${project.title} - ${fileName}`;
-                        projectGallery.appendChild(imgElement);
-                    }
-                });
+    // Fetch all images from the folder
+    fetch(`/paints/${folder}/`)
+        .then(response => response.text())
+        .then(text => {
+            const parser = new DOMParser();
+            const htmlDocument = parser.parseFromString(text, "text/html");
+            const imageElements = htmlDocument.querySelectorAll("a");
+            imageElements.forEach(element => {
+                const fileName = element.getAttribute("href");
+                if (fileName.match(/\.(jpg|jpeg|png|gif)$/)) {
+                    const imgElement = document.createElement('img');
+                    imgElement.src = `/paints/${folder}/${fileName}`;
+                    imgElement.alt = `${folder} - ${fileName}`;
+                    projectGallery.appendChild(imgElement);
+                }
             });
-    } else {
-        document.getElementById('project-info').textContent = 'Projet non trouvé.';
-    }
+        });
 });
